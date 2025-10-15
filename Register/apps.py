f04517582e57fd0a08336ef243de7fb43200b6d6
JAAAACCCCCCKKKK <15233552004@163.com>
2025-10-15 10:35:37 +0100

    fix: signal activation

--- Register/apps.py
+++ Register/apps.py
@@ -1,10 +1,11 @@
 from django.apps import AppConfig
 
-def ready(self):
-    import Register.signals
-
 class RegisterConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Register'
+
+    def ready(self):
+        import Register.signals  # noqa
+
 
 
