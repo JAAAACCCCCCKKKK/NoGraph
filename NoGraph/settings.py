5e848761aa038d0042d394bc240b94326ae0218a
binaryYuki <60097976+binaryYuki@users.noreply.github.com>
2025-10-16 15:14:39 +0100

    feat: add python-dotenv dependency and configure dotenv loading in settings.py

--- NoGraph/settings.py
+++ NoGraph/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.environ['SECRET_KEY']
 JWT_KEY = SECRET_KEY
 JWT_ALG = 'HS256'
 JWT_EXP = 3600
